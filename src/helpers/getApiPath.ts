5babe10a86064179148222475278ee3d48d9d6a4
siorjik <serlkaplun@gmail.com>
2024-03-08 14:39:48 +0200

    improved condition for api url

--- src/helpers/getApiPath.ts
+++ src/helpers/getApiPath.ts
@@ -1,6 +1,6 @@
 const apiHost = process.env.API_HOST
-const appHost = process.env.NEXT_PUBLIC_APP_HOST ?? 'https://language-for-you.online'
-console.log(appHost)
+const appHost = process.env.NEXT_PUBLIC_APP_HOST
+
 export default (url: string, isRemoteApi: boolean = false): string => {
-  return isRemoteApi ? `${apiHost}/api/${url}` : `${appHost}/api/${url}`
+  return isRemoteApi ? `${apiHost}/api/${url}` : appHost ? `${appHost}/api/${url}` : `/api/${url}`
 }
