ba96a633098bdb75aa62e7843511a98334359a41
“siorjik” <serjkaplun@gmail.com>
2024-03-30 14:24:37 +0200

    set up auth routing in middleware, fixed layout width

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,22 @@
-export { default } from 'next-auth/middleware'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const config = { matcher: ['/library/:path*', '/profile'] }
+import getSession from './helpers/getSession'
+import { createAccountAppPath, createPasswordAppPath, loginAppPath } from './utils/paths'
+
+
+export const config = {
+  matcher: ['/', '/library/:path*', '/profile', '/create-password', '/create-account', '/login']
+}
+
+export async function middleware(req: NextRequest) {
+  const session = await getSession(req)
+  const isUnauthorizedPath = req.nextUrl.pathname === loginAppPath ||
+    req.nextUrl.pathname === createAccountAppPath || req.nextUrl.pathname === createPasswordAppPath
+
+  if (session && isUnauthorizedPath) return NextResponse.redirect(req.nextUrl.origin)
+  else if (!session && !isUnauthorizedPath && req.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL(loginAppPath, req.nextUrl.origin))
+  }
+
+  return NextResponse.next()
+}
