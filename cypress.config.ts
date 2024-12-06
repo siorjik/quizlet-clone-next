06ed50732dd3d76bccc50e76ec954c9b117f83e6
siorjik <serjkaplun@gmail.com>
2024-12-06 20:16:33 +0200

    enabled video

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  // video: true,
+  video: true,
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
